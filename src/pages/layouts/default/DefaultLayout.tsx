2d8490e419a370c3ef86cec3366544d2586b2ca7
work-space817 <workspace817@gmail.com>
2023-11-12 13:28:02 +0200

    rebulid without problems file

--- src/pages/layouts/default/DefaultLayout.tsx
+++ src/pages/layouts/default/DefaultLayout.tsx
@@ -1,16 +1,13 @@
 import { Outlet } from "react-router-dom";
 import DefaultHeader from "./DefaultHeader";
 import DefaultNavbar from "./DefaultNavbar";
-import DefaultSidebar from "./DefaultSidebar";
 
 const DefaultLayout = () => {
   return (
     <>
       <div className="container-xxl border">
         <div className="row flex-nowrap pb-sm-0 pb-5">
-          <nav className="d-none d-sm-block sidebar-fonts col-auto col-lg-2">
-            <DefaultSidebar />
-          </nav>
+          <nav className="d-none d-sm-block sidebar-fonts col-auto col-lg-2"></nav>
 
           <div className="col p-3 pt-4 border-start">
             <DefaultHeader />
