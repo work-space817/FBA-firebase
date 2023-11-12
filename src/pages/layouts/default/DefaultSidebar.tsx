c24fb67927ff2d4a5836b63b399911fa93488d52
work-space817 <workspace817@gmail.com>
2023-11-12 13:34:38 +0200

    rebuild with problems sidebar

---
+++ src/pages/layouts/default/DefaultSidebar.tsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+import SidebarSVG from "../../../helpers/selectorsSVG/layoutComponents/SidebarSVG";
+import DefaultNavbar from "./DefaultNavbar";
+
+const DefaultSidebar = memo(() => {
+  return (
+    <>
+      <div className="d-flex flex-column align-items-center  pt-5 text-white min-vh-100 font-Quicksand-Bold">
+        <div className="cloud d-grid text-dark align-items-center">
+          <div>
+            <SidebarSVG id="cloud" />
+          </div>
+          <div className="font-Quicksand-Bold div2 d-none d-lg-inline">
+            FBA -
+          </div>
+          <div className="font-Quicksand-Bold div3 d-none d-lg-inline ">
+            Financial Budgeting App
+          </div>
+        </div>
+
+        <DefaultNavbar />
+        <hr />
+      </div>
+    </>
+  );
+});
+
+export default DefaultSidebar;
